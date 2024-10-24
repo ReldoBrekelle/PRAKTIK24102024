45287cd536a7eabe3d4ae0cdf18cce32307857c1
ReldoBrekelle <rmain405@gmail.com>
2024-10-24 20:13:20 +0700

    Commit dari file_terbaru

--- belajar_subprogram.py
+++ belajar_subprogram.py
@@ -1,6 +1,13 @@
-def mencari_luas_persegi_panjang(panjang,lebar):
-	luas_persegi_panjang = panjang*lebar
+def mencari_luas_persegi_lingkaran(jari-jari):
+	luas_lingkaran = jari-jari*jari-jari
+	return luas_lingkaran
+
+def mencari_luas_persegi_panjang (panjang,lebar):
+	luas_persegi_panjang - panjang*lebar
 	return luas_persegi_panjang
 
-persegi_panjang+pertama = mencari_luas_persegi_panjang(5,10)
+persegi_panjang_pertama = mencari_luas_persegi_panjang(5,10)
 print(persegi_panjang_pertama)
+
+lingkaran_pertama=mencari_luas_lingkaran(5)
+print(lingkaran_pertama)
